Esri/cityengine_for_houdini

commit 679f28b06581d6237e835604479cee57acc48478

HDA file extraction: extract path filtering into util function

src/palladio/Utils.cpp
@@ -45,6 +45,8 @@
 
 namespace {
 
+constexpr std::string_view ILLEGAL_FS_CHARS = "\\/:*?\"<>|";
+
 template <typename inC, typename outC, typename FUNC>
 std::basic_string<outC> callAPI(FUNC f, const std::basic_string<inC>& s) {
 	std::vector<outC> buffer(s.size());
@@ -345,4 +347,13 @@ std::wstring getFileExtensionString(const std::vector<std::wstring>& extensions)
 	if (extensionString.empty())
 		return FILE_ANY;
 	return extensionString;
+}
+
+void ensureNonExistingFile(std::filesystem::path& p) {
+	std::string stem = p.stem().generic_string();
+
+	// filter out common illegal characters from the stem
+	std::replace_if(
+	        stem.begin(), stem.end(), [](char c) { return (ILLEGAL_FS_CHARS.find(c) != std::string::npos); }, '_');
+	p = p.parent_path() / (stem + p.extension().generic_string());
 }

src/palladio/ResolveMapCache.cpp
@@ -70,8 +70,6 @@ ResolveMapCache::KeyType createCacheKey(const std::filesystem::path& rpk) {
 }
 
 #ifndef PLD_TEST_EXPORTS
-constexpr std::string_view ILLEGAL_FS_CHARS = "\\/:*?\"<>|";
-
 std::filesystem::path resolveFromHDA(const std::filesystem::path& p, const std::filesystem::path& unpackPath) {
 	LOG_DBG << "detected embedded resource in HDA: " << p;
 
@@ -79,10 +77,8 @@ std::filesystem::path resolveFromHDA(const std::filesystem::path& p, const std::
 	UT_StringHolder container = getFSReaderFilename(fsr);
 	LOG_DBG << "resource container: " << container;
 
-	auto resName = p.filename().string();
-	std::replace_if(resName.begin(), resName.end(), [](char c) {
-		return (ILLEGAL_FS_CHARS.find(c) != std::string::npos); },'_');
-	std::filesystem::path extractedResource = unpackPath / resName;
+	std::filesystem::path extractedResource = unpackPath / p.filename();
+	ensureNonExistingFile(extractedResource);
 
 	if (fsr.isGood()) {
 		std::filesystem::create_directories(unpackPath);

src/palladio/Utils.h
@@ -109,3 +109,4 @@ inline bool startsWithAnyOf(const std::string& s, const std::vector<std::string>
 }
 
 PLD_TEST_EXPORTS_API std::wstring getFileExtensionString(const std::vector<std::wstring>& extensions);
+void ensureNonExistingFile(std::filesystem::path& p);

commit a17587d9970f0d073c63673fc686afa2a171dfa4

regression fix: filter file name *before* using path functions
otherwise stem() stops at the first colon instead at the last dot

regression introduced in 679f28b0

std::filesystem::path::stem() (or extension()) are sensitive to colon which is a reserved character on Windows NTFS, see https://learn.microsoft.com/en-us/windows/win32/fileio/file-streams

src/palladio/Utils.cpp
@@ -350,12 +350,17 @@ std::wstring getFileExtensionString(const std::vector<std::wstring>& extensions)
 }
 
 void ensureNonExistingFile(std::filesystem::path& p) {
-	std::string stem = p.stem().generic_string();
+	std::string fileName = p.filename().generic_string();
 
-	// filter out common illegal characters from the stem
-	std::replace_if(
-	        stem.begin(), stem.end(), [](char c) { return (ILLEGAL_FS_CHARS.find(c) != std::string::npos); }, '_');
-	p = p.parent_path() / (stem + p.extension().generic_string());
+	// filter out common illegal characters
+	// note: this needs to be done before we use functions like p.stem()
+	//       to correctly handle ':' in Windows paths
+	auto filterChar = [](char c) { return (ILLEGAL_FS_CHARS.find(c) != std::string::npos); };
+	std::replace_if(fileName.begin(), fileName.end(), filterChar, '_');
+
+	auto cleanFileName =  std::filesystem::path(fileName);
+	auto stem = cleanFileName.stem().generic_string();
+	p = p.parent_path() / (stem + cleanFileName.extension().generic_string());
 
 	// ensure we do not produce a collision with existing file
 	for (size_t suf = 0; std::filesystem::exists(p); suf++) {